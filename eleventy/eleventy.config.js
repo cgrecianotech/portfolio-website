85661eba83c1625fba5cf8882f0e40b5e0a33aa7
GlosuU <glosuu31@gmail.com>
2025-08-29 17:38:47 +0300

    Fixed /blog path, part 1

--- eleventy/eleventy.config.js
+++ eleventy/eleventy.config.js
@@ -150,19 +150,18 @@ export const config = {
 		input: "content",          // default: "."
 		includes: "../_includes",  // default: "_includes" (`input` relative)
 		data: "../_data",          // default: "_data" (`input` relative)
-		output: "_site"
+		output: "../blog"
 	},
 
 	// -----------------------------------------------------------------
 	// Optional items:
 	// -----------------------------------------------------------------
 
-	// If your site deploys to a subdirectory, change `pathPrefix`.
+	// Blog deployed to /blog/ within website's root folder
 	// Read more: https://www.11ty.dev/docs/config/#deploy-to-a-subdirectory-with-a-path-prefix
+	pathPrefix: "/blog/",
 
 	// When paired with the HTML <base> plugin https://www.11ty.dev/docs/plugins/html-base/
 	// it will transform any absolute URLs in your HTML to include this
 	// folder name and does **not** affect where things go in the output folder.
-
-	pathPrefix: "/",
 };
